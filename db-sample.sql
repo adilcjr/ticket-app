f7339f3b10486299bb27652bc555b64148a84712
adilcjr <adil.calomeno.jr@gmail.com>
2024-04-29 21:40:02 -0300

    Add database schema and migration files

---
+++ db-sample.sql
@@ -0,0 +1,12 @@
+INSERT INTO Ticket (title, description, status, priority, createdAt, updatedAt)
+VALUES 
+	('Network Connectivity Issue', 'User reports intermittent network disconnections. Investigate and resolve.', 'CLOSED', 'MEDIUM', '2024-04-24 21:30:00', '2024-04-24 21:30:00'),
+	('Software Installation Error', 'User encounters an error while installing software XYZ. Provide assistance.', 'CLOSED', 'HIGH', '2024-04-24 21:45:00', '2024-04-24 21:45:00'),
+	('Printer Not Responding', 'User\'s printer is not printing. Troubleshoot and fix the issue.', 'CLOSED', 'LOW', '2024-04-24 22:00:00', '2024-04-24 22:00:00'),
+	('Email Account Lockout', 'User unable to access email due to account lockout. Reset password and unlock account.', 'STARTED', 'MEDIUM', '2024-04-24 22:15:00', '2024-04-24 22:15:00'),
+	('Application Crash', 'Application XYZ crashes on startup. Investigate and provide a fix.', 'STARTED', 'HIGH', '2024-04-24 22:30:00', '2024-04-24 22:30:00'),
+	('Hardware Replacement', 'User requests replacement of malfunctioning keyboard. Order new keyboard.', 'CLOSED', 'LOW', '2024-04-24 22:45:00', '2024-04-24 22:45:00'),
+	('VPN Connection Issue', 'User unable to establish VPN connection. Troubleshoot and resolve.', 'STARTED', 'MEDIUM', '2024-04-25 10:00:00', '2024-04-25 10:00:00'),
+	('Password Reset Request', 'User forgot password. Reset user account password and notify user.', 'OPEN', 'MEDIUM', '2024-04-25 11:15:00', '2024-04-25 11:15:00'),
+	('Software License Activation', 'User needs assistance with activating software license. Provide guidance.', 'OPEN', 'LOW', '2024-04-25 11:30:00', '2024-04-25 11:30:00'),
+	('Printer Paper Jam', 'User reports paper jam in printer. Clear the jam and ensure proper functioning.', 'OPEN', 'HIGH', '2024-04-25 11:45:00', '2024-04-25 11:45:00');
