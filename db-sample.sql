ddfc63e3048ff7ca43706aef9cbb367ba6e83af5
adilcjr <adil.calomeno.jr@gmail.com>
2024-05-13 08:49:33 -0300

    More registres were added

--- db-sample.sql
+++ db-sample.sql
@@ -9,4 +9,10 @@ VALUES
 	('VPN Connection Issue', 'User unable to establish VPN connection. Troubleshoot and resolve.', 'STARTED', 'MEDIUM', '2024-04-25 10:00:00', '2024-04-25 10:00:00'),
 	('Password Reset Request', 'User forgot password. Reset user account password and notify user.', 'OPEN', 'MEDIUM', '2024-04-25 11:15:00', '2024-04-25 11:15:00'),
 	('Software License Activation', 'User needs assistance with activating software license. Provide guidance.', 'OPEN', 'LOW', '2024-04-25 11:30:00', '2024-04-25 11:30:00'),
-	('Printer Paper Jam', 'User reports paper jam in printer. Clear the jam and ensure proper functioning.', 'OPEN', 'HIGH', '2024-04-25 11:45:00', '2024-04-25 11:45:00');
+	('Printer Paper Jam', 'User reports paper jam in printer. Clear the jam and ensure proper functioning.', 'OPEN', 'HIGH', '2024-04-25 11:45:00', '2024-04-25 11:45:00'),
+	('Network Connectivity Issue', 'User reports intermittent network disconnections. Investigate and resolve.', 'OPEN', 'MEDIUM', '2024-04-26 21:30:00', '2024-04-26 21:30:00'),
+	('Software Installation Error', 'User encounters an error while installing software XYZ. Provide assistance.', 'OPEN', 'HIGH', '2024-04-26 21:45:00', '2024-04-26 21:45:00'),
+	('Printer Not Responding', 'User\'s printer is not printing. Troubleshoot and fix the issue.', 'OPEN', 'LOW', '2024-04-26 22:00:00', '2024-04-26 22:00:00'),
+	('Email Account Lockout', 'User unable to access email due to account lockout. Reset password and unlock account.', 'OPEN', 'MEDIUM', '2024-04-27 22:15:00', '2024-04-27 22:15:00'),
+	('Application Crash', 'Application XYZ crashes on startup. Investigate and provide a fix.', 'OPEN', 'HIGH', '2024-04-28 22:30:00', '2024-04-28 22:30:00');
+	
