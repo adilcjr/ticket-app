f29136211c3fe2c0603645c6912a075cb2417131
adilcjr <adil.calomeno.jr@gmail.com>
2024-06-18 09:38:33 -0300

    Status filter created and added to tickets page.

---
+++ components/StatusFilter.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Status } from "@prisma/client";
+
+const statuses: { label: string; value: string }[] = [
+  { label: "Open / Started", value: "" },
+  { label: "Open", value: Status.OPEN },
+  { label: "Started", value: Status.STARTED },
+  { label: "Closed", value: Status.CLOSED },
+];
+const StatusFilter = () => {
+  const route = useRouter();
+  const searchParams = useSearchParams();
+
+  return (
+    <Select
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        const query = params.size ? `${params.toString()}` : "0";
+        route.push(`/tickets?${query}`);
+      }}
+    >
+      <SelectTrigger className="w-[200px]">
+        <SelectValue placeholder="Filter by status" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {statuses.map((status) => (
+            <SelectItem key={status.value || "0"} value={status.value || "0"}>
+              {status.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default StatusFilter;
